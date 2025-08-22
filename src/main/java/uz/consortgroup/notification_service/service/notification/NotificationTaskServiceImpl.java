26f420a0f789c1a4f9dab8739c24089e6b56b0ef
Doniyor Kurbanov <doniyor.kurbanov.24@gmail.com>
2025-08-22 18:15:41 +0500

    added logback notification_service

--- src/main/java/uz/consortgroup/notification_service/service/notification/NotificationTaskServiceImpl.java
+++ src/main/java/uz/consortgroup/notification_service/service/notification/NotificationTaskServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import uz.consortgroup.core.api.v1.dto.user.enumeration.TaskStatus;
 import uz.consortgroup.core.api.v1.dto.user.request.NotificationCreateRequestDto;
 import uz.consortgroup.notification_service.entity.NotificationTask;
 import uz.consortgroup.notification_service.repository.NotificationTaskRepository;
